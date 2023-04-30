60ac64b2a84c93ef58879ad8fffb798d8d5a19bf
shaharia0 <shaharia35-1964@diu.edu.bd>
2023-04-30 16:30:33 +0600

    added firebase deploy live

--- src/__steps.js
+++ src/__steps.js
@@ -15,3 +15,26 @@
 4. set context value 
 5. set the provider
 */ 
+
+/**
+ * --------Hosting process -------------
+ * 1. one time install for one pc -- npm install -g firebase-tools
+ * 2. firebase login -- firebase login
+ * 
+ * ------- for each project one time ----------
+ * 1. firebase init 
+ * 2. proceed
+ * 3. hosting: firebase up and down arrow and use space bar to select 
+ * 4. existing project 
+ * 5. select the project careful 
+ * 6. which project as public directory: dist(if use  vite) , build(if use create react app)
+ * 7. select single page application
+ * 8. continuos deployment : no 
+ * 9. 
+ */
+/**
+ * ---for every time deploy----
+ * 1. npm run build 
+ * 2. firebase deploy 
+ * 3. here is the firebase live link : ( https://fir-auth-bfd43.web.app )
+ */
